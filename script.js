bac3fc84390188efc5e349fce60ec7310bd8eb5e
yousef badr <yousefbadr10@gmail.com>
2023-12-13 01:11:11 +0200

    OOP second edit

--- script.js
+++ script.js
@@ -5,27 +5,51 @@ function Person(fullName, money, sleepMood, healthRate) {
     this.sleepMood = sleepMood;
     this.healthRate = healthRate;
   }
-Person.prototype.sleep = function(hours) {
-    if (hours === 7) {
-      this.sleepMood = 'happy';
-    } else if (hours < 7) {
-      this.sleepMood = 'tired';
-    } else {
-      this.sleepMood = 'lazy';
+  Object.assign(Person.prototype,{
+    sleep(hours) {
+      if (hours === 7) {
+        this.sleepMood = 'happy';
+      } else if (hours < 7) {
+        this.sleepMood = 'tired';
+      } else {
+        this.sleepMood = 'lazy';
+      }
+    },
+    eat(meals){
+      if (meals === 3) {
+        this.healthRate = 100;
+      } else if (meals === 2) {
+        this.healthRate = 75;
+      } else if (meals === 1) {
+        this.healthRate = 50;
+      }
+    },
+    buy(items){
+      this.money -= items * 10;
     }
-  };
-Person.prototype.eat = function(meals) {
-    if (meals === 3) {
-      this.healthRate = 100;
-    } else if (meals === 2) {
-      this.healthRate = 75;
-    } else if (meals === 1) {
-      this.healthRate = 50;
-    }
-  };
-Person.prototype.buy = function(items) {
-    this.money -= items * 10;
-  };
+  });
+  //another type of syntax 
+// Person.prototype.sleep = function(hours) {
+//     if (hours === 7) {
+//       this.sleepMood = 'happy';
+//     } else if (hours < 7) {
+//       this.sleepMood = 'tired';
+//     } else {
+//       this.sleepMood = 'lazy';
+//     }
+//   };
+// Person.prototype.eat = function(meals) {
+//     if (meals === 3) {
+//       this.healthRate = 100;
+//     } else if (meals === 2) {
+//       this.healthRate = 75;
+//     } else if (meals === 1) {
+//       this.healthRate = 50;
+//     }
+//   };
+// Person.prototype.buy = function(items) {
+//     this.money -= items * 10;
+//   };
   const person1 = new Person("Youssef badr", 1000, '', 0);
   person1.sleep(8);
   person1.eat(2);
